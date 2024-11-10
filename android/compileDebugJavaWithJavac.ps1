b62528ac0e2df585a88de7676e0f7de1a2e85253
iamalbertly <116843037+iamalbertly@users.noreply.github.com>
2024-11-10 17:05:52 +0300

    almost working set up

---
+++ android/compileDebugJavaWithJavac.ps1
@@ -0,0 +1,24 @@
+# Save this script as "RunBuildInfo.ps1" and run it in PowerShell
+# Ensure PowerShell is running with appropriate permissions to execute the script
+./gradlew --stop
+# Clear the screen
+cls
+
+# Define a function to run each command and display it in a terminal-like format
+function Run-Command {
+    param (
+        [string]$Command
+    )
+    # Display the command as if typed into the console
+    Write-Output "`nPS C:\Users\hp\Documents\GitHub\DriveWise> $Command"
+    # Execute the command
+    Invoke-Expression $Command
+}
+
+# Run commands in sequence as if they were typed into the terminal
+Run-Command "Get-Content ..\package.json"
+Run-Command "Get-Content .\settings.gradle"
+Run-Command "Get-Content .\gradle.properties"
+Run-Command "Get-Content .\build.gradle"
+Run-Command "Get-Content .\app\build.gradle"
+Run-Command "./gradlew :app:compileDebugJavaWithJavac --info"
---
+++ android/logs.ps1
@@ -0,0 +1,23 @@
+# Save this script as "RunBuildInfo.ps1" and run it in PowerShell
+# Ensure PowerShell is running with appropriate permissions to execute the script
+
+# Clear the screen
+
+# Define a function to run each command and display it in a terminal-like format
+function Run-Command {
+    param (
+        [string]$Command
+    )
+    # Display the command as if typed into the console
+    Write-Output "`nPS C:\Users\hp\Documents\GitHub\DriveWise> $Command"
+    # Execute the command
+    Invoke-Expression $Command
+}
+
+# Run commands in sequence as if they were typed into the terminal
+Run-Command "Get-Content ..\package.json"
+Run-Command "Get-Content ..\react-native.config.js"
+Run-Command "Get-Content .\settings.gradle"
+Run-Command "Get-Content .\gradle.properties"
+Run-Command "Get-Content .\build.gradle"
+Run-Command "Get-Content .\app\build.gradle"
